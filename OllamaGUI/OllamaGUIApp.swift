46515186ff25d34fe03629786a029059bc72b086
fordfisher <ford@openmy.zip>
2025-02-21 17:08:16 +0100

    Initial Commit

---
+++ OllamaGUI/OllamaGUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OllamaGUIApp.swift
+//  OllamaGUI
+//
+//  Created by Frank Zappa on 21.02.25.
+//
+
+import SwiftUI
+
+@main
+struct OllamaGUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
